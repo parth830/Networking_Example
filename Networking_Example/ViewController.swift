adee96d2155fea18e191c7b777002258ee4a82c8
Parth Patel <parth@Parths-MacBook-Pro.local>
2018-09-28 10:59:07 -0500

    Initial Commit

---
+++ Networking_Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Networking_Example
+//
+//  Created by Ayaan Ruhi on 9/28/18.
+//  Copyright © 2018 parth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
